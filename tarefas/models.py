7be45da65933a7b251a5332b6e56decb2f7e38a4
ThyagoFrancisco <thyagofranciscog@hotmail.com>
2025-08-09 21:30:58 -0300

    Add tarefas app with basic CRUD functionality
    Introduces the 'tarefas' Django app with a Tarefa model, admin registration, forms, views for listing and creating tasks, and corresponding templates. Updates project URLs to include tarefas routes. Initial migration and supporting files are included.

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True)
+    concluida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tarefa
+from .forms import TarefaForm
 
 # Create your views here.
+
+def lista_tarefas(request):
+    tarefas = Tarefa.objects.all()
+    return render(request, 'tarefas/lista.html', {'tarefas': tarefas})
+
+def nova_tarefa(request):
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_tarefas')
+    else:
+        form = TarefaForm()
+    return render(request, 'tarefas/nova.html', {'form': form})
